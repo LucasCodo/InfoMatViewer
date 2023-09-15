e12327fba312867af76605a6a78752de9db4178e
Lucas Santos Silva <luss1010@gmail.com>
2023-09-15 11:06:42 -0300

    adding some endpoints

--- database.py
+++ database.py
@@ -69,6 +69,7 @@ class Review(BaseModel):
 
 class InfoMatList(BaseModel):
     name = TextField()
+    user = ForeignKeyField(Users, backref='infoMatLists')
 
 
 class ListInforMats(BaseModel):
--- main.py
+++ main.py
@@ -15,3 +15,26 @@ templates = Jinja2Templates(directory="templates")
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request,
                                                      "admin_email": AppSettings().admin_email})
+
+
+@app.get("/informational-material")
+async def info_mat():
+    return {}
+
+
+@app.get("/informational-material/details")
+async def info_mat():
+    return {}
+
+
+@app.get("/list-informational-material/{cod}")
+async def info_mat(cod: int):
+    print(cod)
+    return {}
+
+
+@app.get("/list-informational-material")
+async def info_mat():
+    return {}
+
+

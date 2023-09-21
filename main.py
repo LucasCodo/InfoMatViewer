7dd767adec75794aa8cbaeacb1a90b6a7fe7bb28
Lucas Santos Silva <luss1010@gmail.com>
2023-09-21 11:15:33 -0300

    Update endpoints

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from configs import AppSettings
+import database
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,14 +18,14 @@ async def read_item(request: Request):
                                                      "admin_email": AppSettings().admin_email})
 
 
-@app.get("/informational-material")
+@app.get("/informational-material/{info_mat_id}")
 async def info_mat():
     return {}
 
 
-@app.get("/informational-material/details")
-async def info_mat():
-    return {}
+@app.get("/informational-material/{info_mat_id}/details")
+async def info_mat(info_mat_id: int):
+    return database.read_info_mat(info_mat_id)
 
 
 @app.get("/list-informational-material/{cod}")

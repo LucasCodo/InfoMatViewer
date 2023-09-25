2f6a6c084f55fad85a1c02e4c8a5ae0a5530279c
Lucas Santos Silva <luss1010@gmail.com>
2023-09-25 09:24:00 -0300

    Create response_models.py

---
+++ response_models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class InfoMat(BaseModel):
+    title: str
+    author: list
+    publication_year: str
+    cover_image: str  # capa
+    abstract: str  # resumo
+    matters: list  # assuntos
+    tags: list  # tags
+    number_of_pages: str
+    isbn: str
+    issn: str
+    typer: str  # Tipo de material
+    language: str
+    publisher: str  # Editora
+    volume: int
+    series: str
+    edition: str
+    reprint_update: str

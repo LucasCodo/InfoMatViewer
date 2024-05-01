2474983bf033d6e3962f44ee6b2c3d027b371195
Lucas Santos Silva <luss1010@gmail.com>
2024-05-01 14:04:12 -0300

    refactor: changing to oauthorizationcodebearer

--- app/auth.py
+++ app/auth.py
@@ -1,34 +1,42 @@
 from cachetools import cached, TTLCache
 from fastapi import Depends, HTTPException
-from fastapi.security.oauth2 import OAuth2PasswordBearer
-from google.auth.transport import requests
-from google.oauth2 import id_token
-
-from app.configs import APPSETTINGS
 from app.database import create_user, get_permissions
 from app.response_models import User
+from fastapi.security import OAuth2AuthorizationCodeBearer
+import requests
 
 
 @cached(TTLCache(maxsize=256, ttl=1800))
-def verify_google_token(token: str = Depends(OAuth2PasswordBearer(tokenUrl="token"))) -> User:
-    try:
-        # Verificar o token do Google
-        id_info = id_token.verify_oauth2_token(token, requests.Request(),
-                                               APPSETTINGS.GOOGLE_CLIENT_ID)
+def verify_google_token(token: str = Depends(OAuth2AuthorizationCodeBearer(
+    authorizationUrl="https://accounts.google.com/o/oauth2/auth",
+    tokenUrl="https://oauth2.googleapis.com/token",
+    scopes={
+        "email": "Access your email address",
+        "profile": "Access your basic profile information",
+        "openid": ""
+    }
+))) -> User:
+    token_info_url = "https://www.googleapis.com/oauth2/v1/tokeninfo"
 
-        # Verificar se o token é válido para a sua aplicação
-        if id_info['aud'] != APPSETTINGS.GOOGLE_CLIENT_ID:
-            raise HTTPException(status_code=401, detail="Token inválido")
-        org = id_info.get("hd", None)
-        if org not in ["ufma.br", "discente.ufma.br"]:
-            raise HTTPException(status_code=401, detail="The user is not part of this organization")
-        user = create_user(id_info['email'])
+    # Parâmetros da solicitação para a API
+    params = {'access_token': token}
+
+    # Faz a solicitação para a API
+    response = requests.get(token_info_url, params=params)
+    # Verifica se a solicitação foi bem-sucedida
+    if response.status_code == 200:
+        # Se a resposta foi bem-sucedida, retorna os dados
+        token_info = response.json()
+        email: str = token_info.get("email", "")
+        if not (email.endswith("ufma.br") or email.endswith("discente.ufma.br")):
+            raise HTTPException(status_code=401,
+                                detail="The user is not part of this organization")
+        user = create_user(email)
         if user.disable:
             raise HTTPException(status_code=401)
-
-        # Retornar informações do usuário, se necessário
-        id_info["permissions"] = list(get_permissions(user))
-        id_info["id"] = user.id
-        return id_info
-    except ValueError as _e:
-        raise HTTPException(status_code=401, detail="Token inválido")
+            # Retornar informações do usuário, se necessário
+        token_info["permissions"] = list(get_permissions(user))
+        token_info["id"] = user.id
+        return token_info
+    else:
+        raise HTTPException(status_code=response.status_code)
